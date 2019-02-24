1aa10a700d8149ae45e68669c825a422a3a36bcc
kndig <rebecca.kuendig@edubs.ch>
2019-02-24 20:34:42 +0100

    Tried things

--- README.md
+++ README.md
@@ -3,19 +3,15 @@ Play Memeory!
 ![Memorykarte](https://github.com/GBBasel/Memeory2/blob/master/src/card/back.png)
 A memory with memes.
 
-Das Ziel dieses Projektes ist es, ein Memory zu erstellen, das Memes (Bilder aus dem Internet, die verwendet werden, um eine Meinung oder einen Sachverhalt humorvoll darzustellen) als Kartenbilder verwendet. Diese können durch anklicken aufgedeckt werden. Wenn zwei Karten aufgedeckt wurden, wird kontrolliert, ob es sich um die Gleichen handelt. Ist dies der Fall, bleiben die Karten aufgedeckt, wenn nicht, drehen sie sich wieder um.
+Das Ziel dieses Projektes ist es, ein Memory zu erstellen, das Memes (Bilder aus dem Internet, die verwendet werden, um eine Meinung oder einen Sachverhalt humorvoll darzustellen) als Kartenbilder verwendet. Diese kÃ¶nnen durch anklicken aufgedeckt werden. Wenn zwei Karten aufgedeckt wurden, wird kontrolliert, ob es sich um die Gleichen handelt. Ist dies der Fall, bleiben die Karten aufgedeckt, wenn nicht, drehen sie sich wieder um.
 
-Während der Arbeit sind wir auf folgende, grosse Aufgaben gestossen:
+WÃ¤hrend der Arbeit sind wir auf folgende, grosse Aufgaben gestossen:
 
 * Randomized Gitter (Funktionelle Programmierung und Zufallszahlen sind nicht die besten Freunde)
-* Wie müssen Bilder eingefügt werden? (Vor allem ein Problem in Hissicht auf Aufgabe 1)
+* Wie mÃ¼ssen Bilder eingefÃ¼gt werden? (Vor allem ein Problem in Hinsicht auf Aufgabe 1)
 * on-Click Events ( Wiederum in Hinsicht auf Aufgaben 1 und 2)
 * Richtige Antwort erkennen (Komplett andere Syntax im Vergleich zu Python)
 
 
 ## Randomized Gitter
-Dieses orientiert sich zunächst am aufbau einer HTML-Tabelle. Die _TDs_ werden Card genannt, die _TRs_ sind Rows und _table_ wird zu Gameboard. 
-
-
-
-
+Dieses orientiert sich zunÃ¤chst am Aufbau einer HTML-Tabelle. Die _TDs_ werden Card genannt, die _TRs_ sind Rows und _table_ wird zu Gameboard.
--- src/main.elm
+++ src/main.elm
@@ -11,7 +11,9 @@ import Random.List
 
 type alias Card =
     { id: Int
-    , pic: Int}
+    , pic: Int
+    , flipped: Bool
+    }
 
 type alias BoardRow = Array.Array Card
 type alias GameBoard = Array.Array BoardRow
@@ -24,14 +26,13 @@ createBoard x y function =
 
 displayFunc : Card -> Html Msg
 displayFunc le_cell =
-      img [src ("card/" ++ String.fromInt le_cell.pic ++ ".png")] []
+      if le_cell.flipped == True then
+        img [src ("card/" ++ String.fromInt le_cell.pic ++ ".png")] []
+      else
+        img [src "card/back.png", onClick <| Flip le_cell] []
     --Kombination aus:
     --Html.td [] [Html.text (String.fromInt le_cell.pic)]
     --img [src "card/4.png"] []
-
-displayBack : Card -> Html Msg
-displayBack le_cell =
-      img [src "card/back.png"] []
 
 displayRow : (Card -> Html Msg) -> BoardRow -> Html Msg
 displayRow function le_row =
@@ -50,34 +51,46 @@ initialModel : () -> (Model, Cmd Msg)
 initialModel _ =
     ({ board = repeat 16 '_'
     , player = 1
-    , test_board = createBoard 4 4 (\n -> {id=n, pic=0})}
+    , test_board = createBoard 4 4 (\n -> {id=n, pic=0, flipped=False})}
     , Random.generate SetCards (Random.List.shuffle (List.append (List.range 0 7) (List.range 0 7)))
     )
 
 type Msg
     = Play Int
     | SetCards (List Int)
+    | Flip Card
 
 setModel : List Int -> flags -> (Model, Cmd Msg)
 setModel list _ =
     ({ board = repeat 16 '_'
     , player = 1
-    , test_board = createBoard 4 4 (\n -> {id=n, pic= Array.get n (Array.fromList list) |> Maybe.withDefault 0})}
+    , test_board = createBoard 4 4 (\n -> {id=n, flipped=False, pic= Array.get n (Array.fromList list) |> Maybe.withDefault 0})}
     , Cmd.none
     )
+
+setFlipped : Model -> Card -> (Model, Cmd Msg)
+setFlipped model card =
+      (model, Cmd.none)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         Play n ->
             ({ model | board = set n 'X' model.board}, Cmd.none)
+
+        Flip card ->
+            ({ model | test_board = set 1 True test_board.card}, Cmd.none)
+            --setFlipped model card
+
         SetCards list ->
             setModel list 0
 
 view : Model -> Html Msg
 view model =
     div []
-        [ text "Memeory"
+        [ p []
+            [text "Memeory"
+            ]
         , displayBoard displayFunc model.test_board
         ]
 
